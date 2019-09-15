ca87a3fc71879e59ea8ae40188a8af0809f7e42c
Leigh Forrest <leighmforrest@gmail.com>
2019-09-15 10:25:03 -0400

    Updates

--- precache-manifest.29c81fde638610c3d45f769eeecd13de.js
+++ precache-manifest.29c81fde638610c3d45f769eeecd13de.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "743a815154e61fc5f4c6a41f6b42b966",
+    "revision": "556ea49e7f214b31f3d1ee6bcb9f7b40",
     "url": "/monsters-rolodex/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/monsters-rolodex/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "0de2bcf85bf3f0c826c7",
-    "url": "/monsters-rolodex/static/css/main.908832b7.chunk.css"
+    "revision": "c9e32cf71b3b26f6df18",
+    "url": "/monsters-rolodex/static/css/main.23937aa3.chunk.css"
   },
   {
     "revision": "a03adf93552741389c80",
     "url": "/monsters-rolodex/static/js/2.8f81bce1.chunk.js"
   },
   {
-    "revision": "0de2bcf85bf3f0c826c7",
-    "url": "/monsters-rolodex/static/js/main.6571c973.chunk.js"
+    "revision": "c9e32cf71b3b26f6df18",
+    "url": "/monsters-rolodex/static/js/main.def93266.chunk.js"
   },
   {
     "revision": "c529e0b9dd1c97384ab1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.2969aa870c24526ee04ffcf9ad322494.js"
+  "/monsters-rolodex/precache-manifest.29c81fde638610c3d45f769eeecd13de.js"
 );
 
 self.addEventListener('message', (event) => {
