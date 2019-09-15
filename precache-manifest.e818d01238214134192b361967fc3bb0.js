398ab71715a75dbbe2794ef91d144c3c7948060d
Leigh Forrest <leighmforrest@gmail.com>
2019-09-15 05:21:36 -0400

    Updates

---
+++ precache-manifest.e818d01238214134192b361967fc3bb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143d9860916eb744018d6413bd9f872c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a03adf93552741389c80",
+    "url": "/monsters-rolodex/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "31f1d42248d9bf860b0d",
+    "url": "/monsters-rolodex/static/css/main.8b9c28a9.chunk.css"
+  },
+  {
+    "revision": "a03adf93552741389c80",
+    "url": "/monsters-rolodex/static/js/2.8f81bce1.chunk.js"
+  },
+  {
+    "revision": "31f1d42248d9bf860b0d",
+    "url": "/monsters-rolodex/static/js/main.6571c973.chunk.js"
+  },
+  {
+    "revision": "c529e0b9dd1c97384ab1",
+    "url": "/monsters-rolodex/static/js/runtime~main.b8559ffb.js"
+  }
+]);
